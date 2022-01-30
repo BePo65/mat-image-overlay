f908716150c114ca6bcda536dd54bc10742ff00c
Bernhard Pottler <bpottler@nexgo.de>
2022-01-30 07:41:16 +0100

    test: make app tests use MatImageOverlayHarness

--- projects/mat-image-overlay-demo/src/app/app.component.spec.ts
+++ projects/mat-image-overlay-demo/src/app/app.component.spec.ts
@@ -1,17 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatImageOverlayModule } from 'mat-image-overlay';
+import { MatImageOverlayHarness } from 'mat-image-overlay/testing';
 
 import { AppComponent } from './app.component';
 import { EnumToArrayPipe } from './shared/enum-to-array.pipe';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let loader: HarnessLoader;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         EnumToArrayPipe
@@ -24,19 +30,29 @@ describe('AppComponent', () => {
         MatFormFieldModule,
         MatSelectModule
       ]
-    }).compileComponents();
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+
+    expect(app).not.toBeUndefined();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.actions')?.textContent?.substring(0, 33)).toContain('Click on one of the images above');
+    const dom = fixture.nativeElement as HTMLElement;
+    expect(dom.querySelector('.actions')?.textContent?.substring(0, 33)).toContain('Click on one of the images above');
+  });
+
+  it('should load image overlay', async () => {
+    fixture.componentInstance.openImageOverlay();
+    const overlayHarness = await loader.getHarness(MatImageOverlayHarness);
+    expect(overlayHarness).not.toBeUndefined();
+
+    await expectAsync(overlayHarness.overlayIsLoaded()).withContext('Image overlay is loaded').toBeResolvedTo(true);
   });
 });
