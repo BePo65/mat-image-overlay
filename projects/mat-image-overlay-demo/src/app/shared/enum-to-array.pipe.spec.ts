e10a3e54b1418f8952c3a4bf5ab491005952d5a5
Bernhard Pottler <bpottler@nexgo.de>
2022-02-07 19:06:43 +0100

    build: add new plugins to eslint
    Plugins:
    eslint-plugin-import
    eslint-import-resolver-typescript
    eslint-plugin-prefer-arrow

--- projects/mat-image-overlay-demo/src/app/shared/enum-to-array.pipe.spec.ts
+++ projects/mat-image-overlay-demo/src/app/shared/enum-to-array.pipe.spec.ts
@@ -3,6 +3,7 @@ import { EnumToArrayPipe } from './enum-to-array.pipe';
 describe('EnumToArrayPipe', () => {
   it('create an instance', () => {
     const pipe = new EnumToArrayPipe();
+
     expect(pipe).toBeTruthy();
   });
 });
