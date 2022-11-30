53a173be895bcfcebe9c8159a3d71cafa6c492d2
Bernhard Pottler <bpottler@nexgo.de>
2022-11-30 08:59:38 +0100

    build: update packages to angular v15

--- projects/mat-image-overlay-demo/src/app/app.module.ts
+++ projects/mat-image-overlay-demo/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
 
 import { AppComponent } from './app.component';
 import { MatImageOverlayModule } from 'mat-image-overlay';
